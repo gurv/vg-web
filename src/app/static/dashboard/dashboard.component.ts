544dff0d8bae89312913814c33ef99c762216a9d
gurv <vladimir.gurinovich@gmail.com>
2019-03-08 13:22:10 +0300

    fix (lint): Style 05-02

--- src/app/static/dashboard/dashboard.component.ts
+++ src/app/static/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'dashboard',
+  selector: 'app-dashboard',
   styleUrls: ['./dashboard.component.scss'],
   templateUrl: './dashboard.component.html'
 })
