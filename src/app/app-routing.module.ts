0a2454ca2fbec1562e66905a3a3a63dfce84cb7c
gurv <vladimir.gurinovich@gmail.com>
2020-11-21 20:55:15 +0300

    clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OperationComponent } from './operation/operation.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard'
-  }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
